c82beee854f0d6cbed3208928ba9dead70308741
yesicajv <yjv15.yjv@gmail.com>
2021-10-14 13:45:06 -0700

    modificacion en update2.php

--- update2.php
+++ update2.php
@@ -4,7 +4,6 @@ validar();
 ?>
 <?php 
     $query = "UPDATE table1 SET column2 = '".$_POST['nombre']."', column3 = '".$_POST['fecha']."', column4 = ".$_POST['numero'].", column5 = ".$_POST['numdouble']." WHERE column1 = ".$_POST['identificador'].";";
-//echo $query;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if ( $conn->query($query)== TRUE){
+if ( $conn->query($query)){
     header("location:crud.php");
 }else{
     echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
